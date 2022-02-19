825ac18c720e624c46b1432f53abcbd76d9573d1
franckDev21 <tutodevphp@gmail.com>
2022-02-19 03:22:44 +0100

    first commit

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,24 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <button>cliquer ici</button>
   </div>
 </template>
+
+<style lang="scss" scoped>
+  .about{
+    button{
+      padding: 1rem 2rem;
+      background-color: rebeccapurple;
+      border-radius: .5rem;
+      color: #fff;
+      border: none;
+      outline: none;
+      font-size: 2rem;
+      cursor: pointer;
+      &:active{
+        transform: scale(.93);
+      }
+    }
+  }
+</style>
